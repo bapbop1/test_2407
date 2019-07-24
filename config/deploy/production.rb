eae7f05d039f964313b862bd7c6911083f6f34b4
bapbop1 <hungnx96@gmail.com>
2019-07-24 22:16:14 +0700

    update

--- config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-set :stage, :production
-set :rails_env, :production
-set :deploy_to, "/home/deploy/apps/spree_deploy"
-set :branch, :master
-server "13.250.113.44", user: "deploy", roles: %w(web app db)
